ccf393d13128d50aeed94c008eee00db09aa59ba
tengmo3210 <tengmo02@gmail.com>
2019-05-21 11:47:27 +0700

    remove some  container

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 /// geo graph
-google.charts.load('current', {
-    'packages':['geochart'],
-    // Note: you will need to get a mapsApiKey for your project.
-    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-  });
-  google.charts.setOnLoadCallback(drawRegionsMap);
-  function drawRegionsMap() {
-    var data = google.visualization.arrayToDataTable([
-      ['Country', 'Popularity'],
-      ['Germany', 200],
-      ['United States', 300],
-      ['Brazil', 400],
-      ['Canada', 500],
-      ['France', 600],
-      ['RU', 700]
-    ]);
+// google.charts.load('current', {
+//     'packages':['geochart'],
+//     // Note: you will need to get a mapsApiKey for your project.
+//     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+//     'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+//   });
+//   google.charts.setOnLoadCallback(drawRegionsMap);
+//   function drawRegionsMap() {
+//     var data = google.visualization.arrayToDataTable([
+//       ['Country', 'Popularity'],
+//       ['Germany', 200],
+//       ['United States', 300],
+//       ['Brazil', 400],
+//       ['Canada', 500],
+//       ['France', 600],
+//       ['RU', 700]
+//     ]);
 
-    var options = {};
+  //   var options = {};
 
-    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+  //   var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
-    chart.draw(data, options);
-  }
+  //   chart.draw(data, options);
+  // }
 
   /// line graph
   google.charts.load('current', {packages: ['corechart', 'line', 'bar']});
-  google.charts.setOnLoadCallback(drawBasic);
+  // google.charts.setOnLoadCallback(drawBasic);
   google.charts.setOnLoadCallback(drawChartL);
   google.charts.setOnLoadCallback(drawChartpie_62);
   google.charts.setOnLoadCallback(drawChartpie_61);
@@ -43,40 +43,40 @@ google.charts.load('current', {
   // google.charts.setOnLoadCallback(drawChart12);
   // google.charts.setOnLoadCallback(drawChart13);
   
-  function drawBasic() {
+  // function drawBasic() {
   
-        var data = new google.visualization.DataTable();
-        data.addColumn('number', 'X');
-        data.addColumn('number', 'Dogs');
+  //       var data = new google.visualization.DataTable();
+  //       data.addColumn('number', 'X');
+  //       data.addColumn('number', 'Dogs');
   
-        data.addRows([
-          [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-          [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-          [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-          [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-          [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-          [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-          [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-          [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-          [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-          [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-          [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-          [66, 70], [67, 72], [68, 75], [69, 80]
-        ]);
+  //       data.addRows([
+  //         [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+  //         [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+  //         [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+  //         [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+  //         [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+  //         [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+  //         [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+  //         [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+  //         [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+  //         [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+  //         [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+  //         [66, 70], [67, 72], [68, 75], [69, 80]
+  //       ]);
   
-        var options = {
-          hAxis: {
-            title: 'Time'
-          },
-          vAxis: {
-            title: 'Popularity'
-          }
-        };
+  //       var options = {
+  //         hAxis: {
+  //           title: 'Time'
+  //         },
+  //         vAxis: {
+  //           title: 'Popularity'
+  //         }
+  //       };
   
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  //       var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
   
-        chart.draw(data, options);
-      }
+  //       chart.draw(data, options);
+  //     }
       function drawChartL() {
         var data = new google.visualization.DataTable();
         data.addColumn('number', 'ปี');
